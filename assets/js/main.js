9ede21a2241c0df8195805b9e6b7f08096b47d41
マニス++ <64976884+maniz-std@users.noreply.github.com>
2020-09-04 01:01:45 +0700

    update4

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2018/10/19',function(event){
+     jQuery('#clock').countdown('2020/09/12',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
